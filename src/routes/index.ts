82fc6d5ddd0226bd2846c40c54d9e5589efcdd60
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-16 21:52:44 -0300

    Criando controller e route de sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { usersRoutes } from './usersRoutes'
+import { sessionsRoutes } from './sessionsRoutes'
 
 const routes = Router()
 routes.use('/users', usersRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 export { routes }
