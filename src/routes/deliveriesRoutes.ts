ee0f8e93583445e768f0121efb01fb9b9b06da87
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-16 22:58:02 -0300

    Criando middleware de verificação de autorização

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { DeliveriesController } from '@/controllers/deliveriesController'
 import { ensureAuthenticated } from '@/middlewares/ensureAutheticated'
+import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization'
 
 const deliveriesRoute = Router()
 const deliveriesController = new DeliveriesController()
 
-deliveriesRoute.use(ensureAuthenticated)
+deliveriesRoute.use(ensureAuthenticated, verifyUserAuthorization(['sale']))
 deliveriesRoute.post('/', deliveriesController.create)
 
 export { deliveriesRoute }
