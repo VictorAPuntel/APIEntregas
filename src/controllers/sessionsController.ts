967a24ec776238457f0f5d7fd5094ff51cd7935a
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-16 22:04:05 -0300

    Criando validação de email e senha do usuario e instalando o jsonwebtoken

--- src/controllers/sessionsController.ts
+++ src/controllers/sessionsController.ts
@@ -1,7 +1,30 @@
 import { Request, Response, NextFunction } from 'express'
+import { AppError } from '@/utils/AppError'
+import { prisma } from '@/database/prisma'
+import { compare } from 'bcrypt'
+import { z } from 'zod'
 
 class SessionsController {
-  create(request: Request, response: Response, next: NextFunction) {
+  async create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: { email } })
+
+    if (!user) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
+    const passwarodMatched = await compare(password, user.password)
+
+    if (!passwarodMatched) {
+      throw new AppError('Invalid email or password', 401)
+    }
+
     return response.json({ message: 'ok' })
   }
 }
