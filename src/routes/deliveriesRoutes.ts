e186c5628ad5b1a3596dd59ea00e4fd8a0b83654
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-17 22:22:47 -0300

    Atualizando status do delivery

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express'
 import { DeliveriesController } from '@/controllers/deliveriesController'
 import { ensureAuthenticated } from '@/middlewares/ensureAutheticated'
 import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization'
+import { DeliveriesStatusController } from '@/controllers/deliveriesStatusCOntroller'
 
 const deliveriesRoute = Router()
 const deliveriesController = new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController()
 
 deliveriesRoute.use(ensureAuthenticated, verifyUserAuthorization(['sale']))
 deliveriesRoute.get('/', deliveriesController.index)
 deliveriesRoute.post('/', deliveriesController.create)
+
+deliveriesRoute.patch('/:id/status', deliveriesStatusController.update)
 
 export { deliveriesRoute }
